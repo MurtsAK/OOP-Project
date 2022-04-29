e5436d304b9094d1ba25561c1870411741633aa9
Hazikafarooq <84680497+Hazikafarooq@users.noreply.github.com>
2022-04-30 00:46:15 +0500

    code had error, working code.

--- BlueRing.cpp
+++ BlueRing.cpp
@@ -11,15 +11,15 @@ BlueRing::BlueRing(SDL_Renderer *rend, SDL_Texture *ast, SDL_Rect mov) : Enemy(r
 void BlueRing::draw(SDL_Renderer *gRenderer, SDL_Texture *assets)
 {
     // Body is drawn and moves horizontal direction at the speed of 4 units
-    Enemy::draw(gRenderer,assets,src,mover);
+    Enemy::draw(gRenderer, assets, src, mover);
 }
 
-SDL_Rect BlueRing::getSrc() //returns the coordinates
+SDL_Rect BlueRing::getSrc() // returns the coordinates
 {
     return src;
 }
 
-void BlueRing::dropEnemies() //dropping object at a random speed
+void BlueRing::dropEnemies() // dropping object at a random speed
 {
     srand(time(0));
     int randomY;
@@ -27,25 +27,25 @@ void BlueRing::dropEnemies() //dropping object at a random speed
     mover.y += randomY;
 }
 
-int BlueRing::LifeDeduct() //deduction of life if collided
+int BlueRing::LifeDeduct() // deduction of life if collided
 {
     return -1;
 }
 
-SDL_Rect BlueRing::getMov() //getting dimension
+SDL_Rect BlueRing::getMov() // getting dimension
 {
     return mover;
 }
 
-void BlueRing::animateEnemy() //animation when bullet is hit
+void BlueRing::animateEnemy() // animation when bullet is hit
 {
-{
+
     src = {1439, 438, 107, 121};
 }
 
-bool BlueRing::outOfScreen() //function to check if our object has movedd out of screen or not
+bool BlueRing::outOfScreen() // function to check if our object has movedd out of screen or not
 {
-    if (mover.y>700)
+    if (mover.y > 700)
     {
         return true;
     }
